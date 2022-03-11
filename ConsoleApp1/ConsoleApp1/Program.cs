3c4bd90dfdb5cead50d30130ec58a684860310e4
Aurora Bevacqua <98151584+aurorabevacqua05@users.noreply.github.com>
2022-03-11 09:41:51 +0100

    generatore numeri

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace BevacquaVettori1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            const int dimensione = 400;
+            int[] num = new int[dimensione];
+
+            Random generatore = new Random();           
+            
+            for(int i=0; i<dimensione; i++)
+            {
+                int numero = generatore.Next(1, 100);
+                num[i] =numero;
+                Console.WriteLine($"Numero{i}= {numero}");
+
+           
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+}
