26d744086d3dd379057dd214ca6989ad7d8c77f0
Aurora Bevacqua <98151584+aurorabevacqua05@users.noreply.github.com>
2022-03-18 10:45:20 +0100

    esercizio vettori numero 2

---
+++ BevacquaVettori2/BevacquaVettori2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BevacquaVettori2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int dimensione = 100;
+            int[] vett = new int[dimensione];
+
+            Random generatore = new Random();
+            for(int i=0; i<dimensione; i++)
+            {
+                int numero= generatore.Next(1, 100);
+
+               
+                
+                
+                
+                if (numero > 50)
+                {
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.WriteLine("Il numero non è disponibile");
+                }
+            }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+}
