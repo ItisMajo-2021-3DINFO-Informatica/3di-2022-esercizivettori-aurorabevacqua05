bb92d7b8d80dbdd94fa52921497b706b490fb86f
Aurora Bevacqua <98151584+aurorabevacqua05@users.noreply.github.com>
2022-03-25 09:36:06 +0100

    esercizio vettori

---
+++ Bevacqua_EsercizioVettori/Bevacqua_EsercizioVettori/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Bevacqua_EsercizioVettori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int dimensione = 27;
+            decimal[] prodotti = new decimal[dimensione];
+            for(int i=0; i<dimensione; i++)
+            {
+                Console.WriteLine("Digita il prezzo dei prodotti: ");
+                prodotti[i] = Convert.ToDecimal(Console.ReadLine());            
+            }
+
+            for(int i=0; i<dimensione; i++)
+            {
+                decimal sconto = prodotti[i] * 22 / 100;
+
+                decimal prezzoScontato = prodotti[i] - sconto;
+                Console.WriteLine($"");
+            }
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+}
